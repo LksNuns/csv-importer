71ca57f5729648fbff3ad78b8d723f46f1f23016
LksNuns <lksnunss@gmail.com>
2017-02-10 14:11:25 -0200

    create sale model

---
+++ db/migrate/20170210153406_create_sales.rb
@@ -0,0 +1,12 @@
+class CreateSales < ActiveRecord::Migration
+  def change
+    create_table :sales do |t|
+      t.string :buyer
+      t.text :description
+      t.integer :amount, null: false
+      t.integer :price, null: false
+      t.string :address
+      t.string :provider
+    end
+  end
+end
