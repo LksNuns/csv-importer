1731afc82a7aae943a8ae19252eb6dab7e237c31
LksNuns <lksnunss@gmail.com>
2017-02-10 14:17:43 -0200

    add annotate model gem

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: sales
+#
+#  id          :integer          not null, primary key
+#  buyer       :string
+#  description :text
+#  amount      :integer          not null
+#  price       :integer          not null
+#  address     :string
+#  provider    :string
+#
+
 class Sale < ActiveRecord::Base
 end
