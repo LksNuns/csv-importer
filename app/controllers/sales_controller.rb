9806f2c907fe69af0793790e1253402dac88241d
LksNuns <lksnunss@gmail.com>
2017-02-11 00:30:45 -0200

    add import csv action

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,5 +2,15 @@ class SalesController < ApplicationController
 
   def index
   end
-  
+
+  def import_csv
+    sale_parser = SaleCsvParser.new(params[:file].path)
+
+    sale_parser.each_row_params do |row|
+      Sale.create(row)
+    end
+
+    redirect_to sales_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'sales#index'
 
-  resources :sales, only: [:index]
+  resources :sales, only: [:index] do
+    post :import_csv, on: :collection
+  end
 end
