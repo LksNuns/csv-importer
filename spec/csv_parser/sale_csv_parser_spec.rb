b5adfefae5166463a50c13947502b82b675634ed
LksNuns <lksnunss@gmail.com>
2017-02-12 18:14:58 -0200

    add tests to valid? method

--- spec/csv_parser/sale_csv_parser_spec.rb
+++ spec/csv_parser/sale_csv_parser_spec.rb
@@ -1,40 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe SaleCsvParser do
-  let(:sale_parser) { SaleCsvParser.new("spec/support/files/sale_data.csv").open_csv }
+  let(:parser) { SaleCsvParser.new("spec/support/files/sale_data.csv") }
+  let(:file_csv) { parser.open_csv }
 
   describe "#open_csv" do
     context "when file exists" do
       it "open a file" do
-        expect(sale_parser.class).to be CSV::Table
+        expect(file_csv.class).to be CSV::Table
       end
     end
   end
 
   describe ".sale_params" do
     context "expect rows with" do
-      before do
-        @sale_params = SaleCsvParser.sale_params(sale_parser.first)
-      end
+      subject!(:sale_params) { SaleCsvParser.sale_params(file_csv.first) }
 
       it "buyer column" do
-        expect(@sale_params[:buyer]).to eq "Fulano de Tal"
+        expect(sale_params[:buyer]).to eq "Fulano de Tal"
       end
       it "description column" do
-        expect(@sale_params[:description]).to eq "R$2.99 balinhas"
+        expect(sale_params[:description]).to eq "R$2.99 balinhas"
       end
       it "amount_centavos column" do
-        expect(@sale_params[:amount]).to eq Money.new(299)
+        expect(sale_params[:amount]).to eq Money.new(299)
       end
       it "quantity column" do
-        expect(@sale_params[:quantity]).to eq 5
+        expect(sale_params[:quantity]).to eq 5
       end
       it "address column" do
-        expect(@sale_params[:address]).to eq "Rua de Casa"
+        expect(sale_params[:address]).to eq "Rua de Casa"
       end
       it "provider column" do
-        expect(@sale_params[:provider]).to eq "Venda da Esquina"
+        expect(sale_params[:provider]).to eq "Venda da Esquina"
       end
+    end
+  end
+
+  describe "#valid?" do
+    it "returns true when have no errors" do
+      parser.error = nil
+
+      expect(parser.valid?).to be true
+    end
+
+    it "returns false when have errors" do
+      parser.error = "Arquivo inválido"
+
+      expect(parser.valid?).to be false
     end
   end
 end
