33bfbdc9fa749c57a01edefeec04023dd5ea8f07
LksNuns <lksnunss@gmail.com>
2017-02-12 11:16:58 -0200

    add CreateSaleFromCsv interactor

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,17 +6,9 @@ class SalesController < ApplicationController
   end
 
   def import_csv
-    sale_parser = SaleCsvParser.new(params[:file].path)
-    valid_parser = sale_parser.each_sale_params { |params| Sale.create(params) }
+    result = CreateSalesFromCsv.call(file: params[:file])
 
-    flash[:error] = "Arquivo inválido." unless valid_parser
+    flash[:error] = result.flash_message if result.failure?
     redirect_to sales_path
   end
-
-  private
-
-  def sale_params
-    params.permit(:file)
-  end
-
 end
